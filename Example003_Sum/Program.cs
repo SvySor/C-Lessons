a4426ab80055d31763b31425adaf532a98207eb2
SvySor <svysor@gmail.com>
2022-11-12 22:58:21 +0300

    Random numbers added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,13 @@
 ﻿// Enter data
-int NumberA = 3;
-int NumberB = 5;
+int NumberA = new Random().Next(1, 10);
+int NumberB = new Random().Next(1, 10);
 // Sum
 int result = NumberA + NumberB;
 // Write data
+Console.Write("NumberA: ");
+Console.WriteLine(NumberA);
+Console.Write("NumberB: ");
+Console.WriteLine(NumberB);
+Console.Write("Result: ");
 Console.WriteLine(result);
+Console.WriteLine(" ");
