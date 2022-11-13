39fcbb4e546b2b4bbad5da41ea3a37f6e1cf1220
SvySor <svysor@gmail.com>
2022-11-13 12:46:41 +0300

    Programm added

--- Example006_FindMax/Program.cs
+++ Example006_FindMax/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Enter data
+int a = 1;
+int b = 24;
+int c = 32;
+int d = 17;
+int e = 8;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Max is: ");
+Console.WriteLine(max);
